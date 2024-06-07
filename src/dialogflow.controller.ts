050a95d91ca4bf410f05edb52333692388d4e916
Thiago Silva <thiagosilva1@unipam.edu.br>
2024-06-07 08:54:42 -0300

    fix(Controller): Alteração no controller para receber os dados do front de melhor forma

--- src/dialogflow.controller.ts
+++ src/dialogflow.controller.ts
@@ -1,18 +1,24 @@
-import { Body, Controller, Post} from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { DialogflowService } from './dialogflow.service';
-import { randomUUID } from 'node:crypto';
 
 @Controller()
 export class DialogflowController {
   constructor(private readonly dialogflowService: DialogflowService) {}
 
   @Post('dialogflow')
-  async postDialogflowResponse(@Body('message') message: string) {
-    
-    const projectId = 'chatbotunipam-ixov'; 
-    const sessionId = randomUUID();
+  async PostDialogflowResponse(
+    @Body('message') message: string,
+    @Body('sessionId') sessionId: string,
+  ) {
+    const projectId = 'chatbotunipam-ixov';
     const languageCode = 'pt-BR';
-    const response = await this.dialogflowService.detectIntent(projectId, message, sessionId, languageCode);
+
+    const response = await this.dialogflowService.detectIntent(
+      projectId,
+      message,
+      sessionId,
+      languageCode,
+    );
 
     return response.fulfillmentMessages[0].text;
   }
